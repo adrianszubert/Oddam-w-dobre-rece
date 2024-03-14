6f5c0e3b1a0ab5d493bb7073b71ee030a321ba96
Adrian Szubert <adriandamianszubert@gmail.com>
2024-03-14 09:37:08 +0100

    added Link from react-scroll

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,13 +1,15 @@
+import { Link } from "react-scroll";
+
 const Navbar = () => {
   return (
           <>
             <nav>
-              <ul>
-                <li><a href="/">Start</a></li>
-                <li><a href="/">O co chodzi?</a></li>
-                <li><a href="/">O nas</a></li>
-                <li><a href="/">Fundacja i organizacje</a></li>
-                <li><a href="/">Kontakt</a></li>
+              <ul className="nav__link-container">
+                <li className="nav__list-item"><Link to="start" smooth duration={500} className="nav__link nav__link--start">Start</Link></li>
+                <li className="nav__list-item"><Link to="what-is-it" smooth duration={500} className="nav__link">O co chodzi?</Link></li>
+                <li className="nav__list-item"><Link to="about-us" smooth duration={500} className="nav__link">O nas</Link></li>
+                <li className="nav__list-item"><Link to="who-we-help" smooth duration={500} className="nav__link">Fundacje i organizacje</Link></li>
+                <li className="nav__list-item"><Link to="contact-us" smooth duration={500} className="nav__link">Kontakt</Link></li>
               </ul>
             </nav>
           </>
