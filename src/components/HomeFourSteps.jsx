50e58c4c58dc9fce4bdec0589f64e66afbebcac1
Adrian Szubert <adriandamianszubert@gmail.com>
2024-03-15 19:16:43 +0100

    organising components structure

--- src/components/HomeFourSteps.jsx
+++ src/components/HomeFourSteps.jsx
@@ -6,32 +6,34 @@ import icon4 from '../assets/Icon-4.svg';
 
 const HomeFourSteps = () => {
   return (
-    <section className="four-columns">
-      <h2>Wystarczą 4 proste kroki</h2>
-      <img
-        src={decoration}
-        alt="a decoration "
+    <section className="four-steps__container">
+      <div className='four-steps__heading-wrapper'>
+        <h2 className='four-steps__heading'>Wystarczą 4 proste kroki</h2>
+        <img className='four-steps__decoration'
+          src={decoration}
+          alt="a decoration "
       />
-      <div className="four-columns__container">
-        <div className="four-columns col1">
-          <img src={icon1} alt="a shirt" />
-          <p>Wybierz rzeczy</p>
-          <p>ubrania, zabawki, sprzęt i inne</p>
+      </div>
+      <div className="four-steps__wrapper">
+        <div className="four-steps col1">
+          <img className='four-steps__icon' src={icon1} alt="a shirt" />
+          <p className='four-steps__title'>Wybierz rzeczy</p>
+          <p className='four-steps__text'>ubrania, zabawki, sprzęt i inne</p>
         </div>
-        <div className="four-columns col2">
-          <img src={icon2} alt="a bag" />
-          <p>Spakuj je</p>
-          <p>skorzystaj z worków na śmieci</p>
+        <div className="four-steps col2">
+          <img className='four-steps__icon' src={icon2} alt="a bag" />
+          <p className='four-steps__title'>Spakuj je</p>
+          <p className='four-steps__text'>skorzystaj z worków na śmieci</p>
         </div>
-        <div className="four-columns col3">
-          <img src={icon3} alt="a bag" />
-          <p>Zdecyduj komu chcesz pomóc</p>
-          <p>wybierz zaufane miejsce</p>
+        <div className="four-steps col3">
+          <img className='four-steps__icon four-steps__icon--3' src={icon3} alt="a magnifier" />
+          <p className='four-steps__title'>Zdecyduj komu<br/> chcesz pomóc</p>
+          <p className='four-steps__text'>wybierz zaufane miejsce</p>
         </div>
-        <div className="four-columns col4">
-          <img src={icon4} alt="a bag" />
-          <p>Zamów kuriera</p>
-          <p>kurier przyjedzie w dogodnym terminie</p>
+        <div className="four-steps col4">
+          <img className='four-steps__icon four-steps__icon--4' src={icon4} alt="a loop" />
+          <p className='four-steps__title'>Zamów kuriera</p>
+          <p className='four-steps__text'>kurier przyjedzie w dogodnym terminie</p>
         </div>
       </div>
       <a href="/">oddaj rzeczy</a>
--- src/components/HomeThreeColumns.jsx
+++ src/components/HomeThreeColumns.jsx
@@ -7,14 +7,14 @@ const HomeThreeColumns = () => {
         <p className="three-columns__text">Lorem ipsum dolor sit amet, consectetur adipisc Pellentesque vel enim a elit viverra elementuma. Aliquam erat volutpat.</p>
       </div>
       <div className="three-columns col2">
-        <p>5</p>
-        <p>wspartych organizacji</p>
-        <p>Lorem ipsum dolor sit amet, consectetur adipisc Pellentesque vel enim a elit viverra elementuma. Aliquam erat volutpat.</p>
+        <p className="three-columns__quantity">5</p>
+        <p className="three-columns__items">wspartych organizacji</p>
+        <p className="three-columns__text">Lorem ipsum dolor sit amet, consectetur adipisc Pellentesque vel enim a elit viverra elementuma. Aliquam erat volutpat.</p>
       </div>
       <div className="three-columns col3">
-        <p>7</p>
-        <p>zorganizowanych zbiórek</p>
-        <p>Lorem ipsum dolor sit amet, consectetur adipisc Pellentesque vel enim a elit viverra elementuma. Aliquam erat volutpat.</p>
+        <p className="three-columns__quantity">7</p>
+        <p className="three-columns__items">zorganizowanych zbiórek</p>
+        <p className="three-columns__text">Lorem ipsum dolor sit amet, consectetur adipisc Pellentesque vel enim a elit viverra elementuma. Aliquam erat volutpat.</p>
       </div>
     </section>
   )
