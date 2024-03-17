8ac24aeee1d052192f5c453b8966a1e4f107dd7a
Adrian Szubert <adriandamianszubert@gmail.com>
2024-03-17 20:57:42 +0100

    separate component created

---
+++ src/components/Decoration.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import decoration from '../assets/Decoration.svg'
+
+const Decoration = () => {
+  return (
+    <img className="decoration" src={decoration} alt="a decoration " />
+  )
+}
+
+export default Decoration;
