56218f8810d5ebb11dec82e564d26d17b24c902c
Adrian Szubert <adriandamianszubert@gmail.com>
2024-03-18 20:55:27 +0100

    organising components structure

--- src/components/HomeContactUs.jsx
+++ src/components/HomeContactUs.jsx
@@ -1,26 +1,39 @@
 import fb from '../assets/Facebook.svg';
 import insta from '../assets/Instagram.svg';
+import Decoration from './Decoration';
 
 const HomeContactUs = () => {
   return (
     <section id="contact-us" className="contact">
-      <form>
-        <label for="name">Wpisz swoje imię</label>
-        <input type="text" id="name" placeholder="Krzysztof"/>
-        <label for="email">Wpisz swój email</label>
-        <input type="email" id="email" placeholder="abc@xyz.pl"/>
-        <label for="message">Wpisz swoją wiadomość</label>
-        <textarea
-          name="message"
-          id="message"
-          cols="30"
-          rows="6"
-          placeholder="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."
-        ></textarea>
-      </form>
-      <footer>
+      <div className='contact-heading-form-wrapper'>
+        <div className='contact-heading-deco-wrapper'>
+          <h2 className='contact-heading'>Skontaktuj się z nami</h2>
+        <Decoration/>
+        </div>
+        <form className='contact-form'>
+          <div className='input-wrapper'>
+            <label className='form-label' for="name">Wpisz swoje imię</label>
+            <input type="text" id="name" placeholder="Krzysztof"/>
+          </div>
+          <div className='input-wrapper'>
+            <label className='form-label' for="email">Wpisz swój email</label>
+            <input type="email" id="email" placeholder="abc@xyz.pl"/>
+          </div>
+          <div className='input-wrapper'>
+            <label className='form-label' for="message">Wpisz swoją wiadomość</label>
+            <textarea
+              name="message"
+              id="message"
+              cols="30"
+              rows="6"
+              placeholder="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."
+            ></textarea>
+          </div>
+        </form>
+      </div>
+      <footer className='contact-footer'>
         <p>Copyright by Coders Lab</p>
-        <div className="social-icons">
+        <div className="contact-footer__social-icons">
           <img
             src={fb}
             alt="a Facebook icon "
--- src/components/HomeHelp.jsx
+++ src/components/HomeHelp.jsx
@@ -1,34 +1,65 @@
-import decoration from '../assets/Decoration.svg';
+import Decoration from "./Decoration";
 
 const HomeHelp = () => {
   return (
     <section id="who-we-help" className="help">
-      <h2>Komu pomagamy?</h2>
-      <img
-        src={decoration}
-        alt="a decoration "
-      />
-      <button>Fundacjom</button>
-      <button>Organizacjom pozarządowym</button>
-      <button>Lokalnym zbiórkom</button>
-      <p>
-        W naszej bazie znajdziesz listę zweryfikowanych Fundacji, z którymi
-        współpracujemy. Możesz sprawdzić czym się zajmują, komu pomagają i czego
+      <h2 className="help__heading">Komu pomagamy?</h2>
+
+      <Decoration />
+
+      <div className="help-buttons-wrapper">
+        <button className="help-button help-button--1">
+          Fundacjom</button>
+        <button className="help-button help-button--2">
+          Organizacjom<br/> pozarządowym
+        </button>
+        <button className="help-button help-button--3">
+          Lokalnym<br/> zbiórkom
+        </button>
+      </div>
+
+      <p className="help-text">
+        W naszej bazie znajdziesz listę zweryfikowanych Fundacji, z<br/> którymi
+        współpracujemy. Możesz sprawdzić czym się zajmują,<br/> komu pomagają i czego
         potrzebują.
       </p>
-      <div></div>
-      <div></div>
-      <div></div>
-      <div class="pagination">
-        <a className="pagination-link" href="/">
-          1
-        </a>
-        <a className="pagination-link" href="/">
-          2
-        </a>
-        <a className="pagination-link" href="/">
-          3
-        </a>
+
+      <div className="help-orgs__container">
+        
+        <div className="help-orgs__row help-orgs__row1">
+          <div className="help-orgs__t-p-wrapper">
+            <h3 className="help-orgs__title">Fundacja “Dbam o Zdrowie”</h3>
+            <p className="help-orgs__purpose">
+              Cel i misja: Pomoc osobom znajdującym się w trudnej sytuacji
+              życiowej.
+            </p>
+          </div>
+          <p className="help-orgs__items">
+            ubrania, jedzenie, sprzęt AGD, meble, zabawki
+          </p>
+        </div>
+
+        <div className="help-orgs__row help-orgs__row2">
+          <div className="help-orgs__t-p-wrapper">
+            <h3 className="help-orgs__title">Fundacja “Dla dzieci”</h3>
+            <p className="help-orgs__purpose">
+              Cel i misja: Pomoc dzieciom z ubogich rodzin.
+            </p>
+          </div>
+          <p className="help-orgs__items">ubrania, meble, zabawki</p>
+        </div>
+
+        <div className="help-orgs__row help-orgs__row3">
+          <div className="help-orgs__t-p-wrapper">
+            <h3 className="help-orgs__title">Fundacja “Bez domu”</h3>
+            <p className="help-orgs__purpose">
+              Cel i misja: Pomoc dla osób nie posiadających miejsca
+              zamieszkania.
+            </p>
+          </div>
+          <p className="help-orgs__items">ubrania, jedzenie, ciepłe koce</p>
+        </div>
+
       </div>
     </section>
   );
